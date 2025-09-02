d362cfb247ceddf5edc84f6177f2ac7f399a9609
juanormo09 <55120371+juanormo09@users.noreply.github.com>
2025-09-01 21:28:53 -0500

    Initial commit

---
+++ Source/Inventory/Inventory.Build.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Inventory : ModuleRules
+{
+	public Inventory(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateIncludePaths.Add("Inventory");
+	}
+}
